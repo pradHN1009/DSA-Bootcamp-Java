ad8872f8a6c6dbeb75235d6de4239ae2c5e7c88c
pradHN1009 <pradyumnahn@gmail.com>
2023-03-26 20:22:03 +0530

    Explored automatic conversions

--- practice/IntellIJ_Projects/Project_1/src/com/prad/Type_conversion.java
+++ practice/IntellIJ_Projects/Project_1/src/com/prad/Type_conversion.java
@@ -8,9 +8,15 @@ public class Type_conversion {
         int num = (int)(67.65f); //type conversion - Float to int
         System.out.println(num);
 
-        //Automatic type conversion in expressions
         int a = 254;
-        byte b = a;
+        byte b = (byte)(a); //254 % 256
         System.out.println(b);
+
+        //Automatic type conversion in expressions
+        byte x = 20;
+        byte y = 30;
+        byte z = 100;
+        int f  = (x * y) / z;
+        System.out.println(f);
     }
 }
