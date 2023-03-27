ac8e63dcf2b938540fd3580eecf6645a71404db7
pradHN1009 <pradyumnahn@gmail.com>
2023-03-27 19:35:59 +0530

    Explored fucntions and some concepts of OOPS

---
+++ practice/IntellIJ_Projects/OOPS/src/Human.java
@@ -0,0 +1,11 @@
+public class Human {
+    public static void main(String[] args)
+    {
+        Human prad = new Human();
+        prad.greet();
+    }
+    void greet(){
+        System.out.println("Hello, How are you?");
+    }
+
+}
